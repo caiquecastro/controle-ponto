c5a5787da378029f469970fa4457ddc644d604fd
Caique de Castro <caiquecastro@outlook.com>
2014-07-30 11:35:58 -0300

    Changed bower directory and created folders for admin and client apps

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-controle-ponto
+Controle de Ponto
 ==============
 
-Tool for time control
+Ferramenta para controlar o ponto de funcionários e estagiários
+
+##Funcionalidades##
+###Administrador###
+* Ver quem está em horário de serviço no momento
+* Ver o relatório de horários de ponto dos seus funcionários
+
+###Cliente###
+* Registrar entrada / saída de horário de serviço
+* Ver seu relatório de horário de ponto
---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Controle de Ponto</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css"> -->
+</head>
+<body>
+    <nav class="navbar navbar-inverse" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Controle de Ponto</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Entrar/Sair</a></li>
+                    <li><a href="#">Relatório de Ponto</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Sair</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
