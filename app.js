855cb2ea5bf84879aa437228a1f91da72babe744
Caique de Castro <caiquecastro@outlook.com>
2017-11-02 17:20:11 -0200

    Add pug as view renderer engine

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.set('views', 'resources/views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(3000);
